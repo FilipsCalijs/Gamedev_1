0c1531d9d396870013a89756b57763c47b2739ba
FilipsCalijs <filips.calijs@gmail.com>
2022-07-01 14:38:02 +0300

    Пули
    добавил анимацию пули, ее полет и смэрть

--- fortbite/game.py
+++ fortbite/game.py
@@ -22,10 +22,11 @@ GRAVITY = 0.55
 
 moving_right = False
 moving_left = False
+shoot = False
 
 #загрузка изоброжений
 
-
+bullet_img = pygame.image.load('img/icons/bullet.png').convert_alpha()
 
 BG = (144,201,120)
 RED = (255,0,0)
@@ -36,15 +37,20 @@ def draw_bg():
 
    
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self,char_type,x,y,scale,speed):
+    def __init__(self,char_type,x,y,scale,speed,ammo ):
         
         pygame.sprite.Sprite.__init__(self)
         self.alive = True
         self.char_type = char_type
         self.speed = speed
         self.jump = False
+        self.ammo = ammo
+        self.start_ammo = ammo
         self.in_air = True
         self.vel_y = 0
+        self.shoot_cooldown = 0
+        self.health = 100
+        self.max_health = self.health
         self.direction = 1
         self.flip = False
         self.animation_list = []
@@ -53,7 +59,7 @@ class Soldier(pygame.sprite.Sprite):
         self.action = 0
 
 
-        animation_types = ["Idle","Run","Jump"]
+        animation_types = ["Idle","Run","Jump","Death"]
         for animation in animation_types:
             #сбросить список изоброжений
             temp_list = []
@@ -61,7 +67,7 @@ class Soldier(pygame.sprite.Sprite):
             num_of_frames = len(os.listdir(rf"B:\promma\python\game\fortbite\img\{self.char_type}\{animation}"))
             for i in range(num_of_frames):
             #f озночает. что если я добавлю мигурные скобки то помещю туда переменную
-                img = pygame.image.load(rf"B:\promma\python\game\fortbite\img\{self.char_type}\{animation}\{i}.png")
+                img = pygame.image.load(rf"B:\promma\python\game\fortbite\img\{self.char_type}\{animation}\{i}.png").convert_alpha()
                 #масштабирыванние
                 img = pygame.transform.scale(img,(int(img.get_width() * scale),int(img.get_height()* scale)))
                 temp_list.append(img)
@@ -73,6 +79,14 @@ class Soldier(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
+    def update(self):
+        self.update_animation()
+        self.check_alive()
+        #обновлять КД
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
+        
+    
     def move(self, moving_left, moving_right): 
         #нужны для проверки с столкновением 
         dx = 0
@@ -86,7 +100,7 @@ class Soldier(pygame.sprite.Sprite):
         if moving_right:
             dx = self.speed
             self.flip = False
-            self.direction = -1
+            self.direction = 1
 
         #прыжок
             
@@ -111,6 +125,16 @@ class Soldier(pygame.sprite.Sprite):
         #обновлять позицию
         self.rect.x += dx
         self.rect.y += dy
+
+
+    def shoot(self):
+            if self.shoot_cooldown == 0 and self.ammo > 0:
+                self.shoot_cooldown = 20
+                bullet = Bullet(player.rect.centerx + (0.6 * self.rect.size[0] * self.direction),self.rect.centery,self.direction )
+                bullet_group.add(bullet)
+                self.ammo -= 1 
+        
+        
     def draw(self):   #
          screen.blit(pygame.transform.flip(self.image,self.flip,False),self.rect)
 
@@ -120,7 +144,10 @@ class Soldier(pygame.sprite.Sprite):
         
          #если анимация подошла к концу, то вернутьсяк старту
         if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
+            if self.action == 3:
+                self.frame_index = len(self.animation_list[self.action]) - 1
+            else:
+                self.frame_index = 0
         #обновлять картинку взависимости от текущего кадра
         self.image = self.animation_list[self.action][self.frame_index]
        
@@ -130,6 +157,11 @@ class Soldier(pygame.sprite.Sprite):
         if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
+        
+
+
+
+            
     def update_action(self,new_action):
         #проверять если новое действие другое чем предыдущее
         if new_action != self.action:
@@ -137,23 +169,73 @@ class Soldier(pygame.sprite.Sprite):
             self.frame_index = 0
             self.update_time = pygame.time.get_ticks()
 
+
+    def check_alive(self):
+        if self.health <= 0:
+            self.health = 0
+            self.speed = 0
+            self.alive = False
+            self.update_action(3)
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 10
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+
+    def update(self):
+        #удалять пули
+        self.rect.x += (self.direction * self.speed)
+        #проверять если пуля ушла за экран
+        if self.rect.right < 0  or self.rect.left > SCREEN_WIDTH:
+            self.kill()
+
+        #проверять сталкевония с персонажами
+        if pygame.sprite.spritecollide(player, bullet_group, False):
+            if player.alive:
+                self.kill()
+                player.health -= 5
+                
+                
+        if pygame.sprite.spritecollide(enemy, bullet_group, False):
+            if enemy.alive:
+                self.kill()
+                enemy.health -= 25
+                
         
-player = Soldier("player",300,200,3,5)
-player2 = Soldier("player",200,100,3,5)    
-enemy = Soldier("enemy",100,90,3,5)
+#создать группу спрйтов
+bullet_group = pygame.sprite.Group()
+
+
+        
+    
+player = Soldier("player",300,200,3,5,20)
+player2 = Soldier("player",200,100,3,5,20)    
+enemy = Soldier("enemy",200,245,3,5,1000)
 run = True
 while run:
     draw_bg()
     clock.tick(FPS)
 
-    player.update_animation()
+    player.update()
     #player2.update_animation()
-    
+
+   
     player.draw()
     #player2.draw()
     player.update_animation()
     enemy.draw()
+    enemy.update()
+
+    bullet_group.update()
+    bullet_group.draw(screen)
+    
     if player.alive:
+        if shoot:
+            player.shoot()
         if player.in_air :
             player.update_action(2)# 2 озночает активацию прыжка
         elif moving_left or moving_right:
@@ -179,6 +261,9 @@ while run:
                 moving_left = True 
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_SPACE:
+                print("space")
+                shoot = True   
             if event.key == pygame.K_w and player.alive:
                 player.jump = True
             if event.key == pygame.K_ESCAPE:
@@ -189,7 +274,10 @@ while run:
             if event.key == pygame.K_a:
                 moving_left = False
             if event.key == pygame.K_d:
-                moving_right = False   
+                moving_right = False
+            if event.key == pygame.K_SPACE:
+                shoot = False
+                print("stop")
     
     pygame.display.update()
 
